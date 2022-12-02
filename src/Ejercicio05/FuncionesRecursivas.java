965459c60788585380d0bb951e0c3c9316b28e83
Lydia <lydia.perez@iesnervion.es>
2022-12-02 18:24:55 +0100

    recursividad

--- src/Ejercicio05/FuncionesRecursivas.java
+++ src/Ejercicio05/FuncionesRecursivas.java
@@ -3,15 +3,31 @@ package Ejercicio05;
 public class FuncionesRecursivas {
     public static int sumatorio(int numero){
         int suma=0;
-        for (int i = 1; i <= numero; i++) {
-            suma+=i;
+        if(numero==1){
+            return 1;
+        }else {
+            suma=suma+sumatorio(numero-1);
         }
         return suma;
     }
     public static double potencia(double a, int n){
-        return Math.pow(a,n);
+        double potencia;
+        if (n==0){
+            return 1;
+        }else {
+            potencia=a*potencia(a, n-1);
+        }
+        return potencia;
     }
     public static int serieFibonacci(int numero){
-        return (numero-1)+(numero-2);
+        int fibonacci=0;
+        if (numero==0||numero==1){
+            fibonacci=numero;
+        } else if (numero<0) {
+            System.out.println("valor no valido");
+        } else {
+            fibonacci=serieFibonacci(numero-1)+serieFibonacci(numero-2);
+        }
+        return fibonacci;
     }
 }
